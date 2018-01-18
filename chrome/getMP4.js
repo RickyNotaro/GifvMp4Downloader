42b4dbe00fd0e725158f43b978a8d186b2720817
Hilal Rizek <hilal.rr.87@gmail.com>
2018-01-19 00:42:53 +0200

    add .mp4 extenstion to filename

--- chrome/getMP4.js
+++ chrome/getMP4.js
@@ -1,1 +1,1 @@
-var l = document.createElement('a'); l.href = document.querySelector('source').src; l.download = document.title; l.click();
+var l = document.createElement('a'); l.href = document.querySelector('source').src; l.download = document.title+'.mp4'; l.click();
